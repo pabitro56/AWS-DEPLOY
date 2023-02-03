c77eba31cb968f208a5b9a36a871db0acccd24ac
pabitro <pabitro.ece@gmail.com>
2023-02-03 09:26:42 +0600

    version added

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+# Terraform block
+terraform {
+  required_version = "~> 1.3.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = "us-east-1"
   access_key = "AKIAWIIGXQ5F4KCXAHYB"
