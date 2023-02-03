be34283e7e70349c0edbd1c055d57f808a2971b8
pabitro56 <60471652+pabitro56@users.noreply.github.com>
2023-02-03 15:47:57 +0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAWIIGXQ5F4KCXAHYB"
-  secret_key = "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"
+  access_key = "AKIAWIIGX"
+  secret_key = "8wuYiMV/7Tqeb"
 }
